0ff96927dc5bcf124a1e7dea8c1e211a132c47d5
Branden <brandenehman45@gmail.com>
2021-12-29 15:16:51 -0700

    Setup sessions controller to handle logging in and out,as well as the ability to create a user with a username and password.

--- movie-app-backend/app/models/user.rb
+++ movie-app-backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_secured_password
     has_many :reviews
     has_many :movies, through: :reviews
+    validates :username, length: {minimum: 4}
 end
--- movie-app-backend/config/routes.rb
+++ movie-app-backend/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :users
-  resources :movies
+  scope :api do
+    resources :reviews
+    resources :users
+    resources :movies
+
+    post "/login", to: "sessions#create"
+    post "/logout", to: "sessions#destroy"
+    
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
